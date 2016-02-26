fc06b181ddc16001e6ba460062c7ff70433ebdf6
Sahaj Sarup <sahajsarup@gmail.com>
2016-02-26 23:56:23 +0530

    Update lcd.py

--- lcd.py
+++ lcd.py
@@ -112,6 +112,13 @@ def main():
 
   # Initialise display
   lcd_init()
+  i = 0
+  while i == 30:
+    lcd_string("    WELCOME     ",LCD_LINE_1)
+    time.sleep(1)
+    lcd_string("wait for"+str(i)+"sec")
+    i++
+    
 
   while True:
     
